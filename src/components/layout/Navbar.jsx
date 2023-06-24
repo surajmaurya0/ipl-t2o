9a4b2bf13d7e72608831cd10c8536799f4ece490
surajmaurya0 <surajdevv1@gmail.com>
2023-06-25 00:13:20 +0530

    login page implement

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,9 +3,14 @@ import { Link } from 'react-router-dom'
 import logo from '../../assest/logo/ipl-logo.png'
 import './navbarStyle.css'
 import { menuState } from '../../recoil/menuRecoil'
-import  {useRecoilState} from 'recoil'
+import { useRecoilState } from 'recoil'
 const Navbar = () => {
-   const [menu,setMenu] = useRecoilState(menuState)
+    const [menu, setMenu] = useRecoilState(menuState)
+    const [logInModal, setLogInModal] = useState()
+    const[logInData,setLogInData] = useState({
+        email:'',
+        password:''
+    })
     return (
         <>
             <nav class="navbar is-transparent">
@@ -29,13 +34,42 @@ const Navbar = () => {
                                 <i class="fa-solid fa-magnifying-glass" style={{ marginLeft: 'auto' }}></i>
                             </div>
                             <div className="buttons">
-                                <Link to="/add-product" className="button is-link btn"><strong>Log in</strong></Link>
+                                <button className="button is-link btn" onClick={()=> setLogInModal('is-active')}><strong>Log in</strong></button>
 
                             </div>
                         </div>
                     </div>
                 </div>
             </nav>
+            <div className={`modal ${logInModal}`} >
+                <div className="modal-background"></div>
+                <div className="modal-card">
+                    <header className="modal-card-head">
+                        <p className="modal-card-title">Log In</p>
+                        <button className="delete" aria-label="close" onClick={()=> setLogInModal('')}></button>
+                    </header>
+                    <section className="modal-card-body">
+                    <div className="card-content">
+                  <div className="columns" style={{display:'block'}}>
+                    <div className="column is-fullwidth">
+                      <input type="text" className="input" placeholder='Enter Email'  value={logInData.email}  onChange={(e)=> setLogInData({...logInData,email:e.target.value})}/>
+                    </div>
+                    
+                    <div className="column is-fullwidth">
+                      <input type="password" className="input" placeholder='Enter Password'  value={logInData.password} onChange={(e)=> setLogInData({...logInData,password:e.target.value})} />
+                    </div>
+
+                    </div>
+                    </div>
+                    </section>
+                    <footer className="modal-card-foot">
+                        <div className="buttons">
+                            <Link className="button is-link btn"><strong>Log in</strong></Link>
+                        <button className="button" onClick={()=> setLogInModal('')}>Cancel</button>
+                        </div>
+                    </footer>
+                </div>
+            </div>
         </>
     )
 }
